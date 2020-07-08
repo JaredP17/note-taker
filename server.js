66fc18aa3118d2ad5c74f05c9db82f09d4f562e7
Jared Porter <jaredp17@me.com>
2020-07-08 02:43:07 -0400

    Added default route if no matching route was found.

--- server.js
+++ server.js
@@ -36,8 +36,13 @@ app.get("/", (req, res) => {
 
 // Notes Route
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// If no matching route is found default to home
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // API/JSON
 
